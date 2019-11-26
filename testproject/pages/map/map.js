bb02fbb7e0ed948b702f59835f1ff07091f3eb2a
zifeng175mo <378663429@qq.com>
2019-11-27 00:04:59 +0800

    Finish the province page.

--- testproject/pages/map/map.js
+++ testproject/pages/map/map.js
@@ -62,5 +62,11 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  pageNavi: function (e) {
+    wx.navigateTo({
+      url: '../province/province?name=' + e.currentTarget.id
+    })
   }
 })
